96974098ff350218d2321db862fd61ae9472e469
Josiah Wong <josiah.wong@homesite.com>
2016-09-28 04:52:03 -0400

    Added code to handle dynamic users

--- src/command/Invite.js
+++ src/command/Invite.js
@@ -12,6 +12,31 @@ var Slack = require('slack-node');
 var User = require('src/model/dynamodb/User.js');
 var _ = require('underscore');
 
+var getChannelName = function() {
+    var channelName = new Date().toISOString().slice(0, 10);
+    return 'Lunch-' + channelName; 
+};
+
+var getSlack = function() {
+    var apiToken = Config.slack.api_token;
+    return new Slack(apiToken);
+};
+
+var normalizeUsersToInvite = function(usersToInvite) {
+    var results = [];
+    if (!usersToInvite) {
+        return results;
+    }
+
+    for (var i = 0; i < usersToInvite.length; i++) {
+        if (usersToInvite[i]) {
+            results[i] = usersToInvite[i].replace('@', '');
+        }
+    }
+
+    return results;
+};
+
 module.exports = BaseCmd.extend({
 
     name: "invite",
@@ -22,48 +47,62 @@ module.exports = BaseCmd.extend({
     },
 
     run: function() {
-
-        console.log('Received options: ', JSON.stringify(this.options.users, null, 2));
-
-        var userList = [];
-        var channelName = new Date().toISOString().slice(0, 10);
-        channelName = 'Lunch-' + channelName;
-
-        apiToken = Config.slack.api_token;
-        console.log('apiToken: %j', apiToken);
-        slack = new Slack(apiToken);
+        var slackUsers = [];
+        var channelName = getChannelName();
+        var slack = getSlack();
+        var usersToInvite = normalizeUsersToInvite(this.options.users);
+        var commandResponseObj = this.response;
 
         async.waterfall([
             function(cb) {
                 slack.api("users.list", function(err, response) {
-                    console.log('slack user list: %j', response.members);
-                    userList = _.indexBy(response.members, 'name');
-                    console.log('indexed userList: %j', userList);
-                    console.log('find user: %j', userList['josiahtest']);
-                    cb(null, userList);
+                    if (err) {
+                        cb(err, 'Unable to retrieve Slack user list');
+                        return;
+                    } else {
+                        console.log('slack user list: %j', response.members);
+                        slackUsers = response.members;
+                        cb(null, null);
+                    }
                 });
             },
             function(result, cb) {
                 slack.api('channels.join', {
                     name: channelName
-                }, function(err, joinResponse){
-                    console.log('channels.join response: %j', joinResponse);
+                }, function(err, response){
+                    console.log('channels.join response: %j', response);
                     if (err) {
-                        callback(null, 'Cannot create channel');
+                        cb(err, 'Cannot create channel');
                         return;
                     }
-                    cb(null, joinResponse);
+                    cb(null, response);
                 });
             },
             function(result, cb) {
-                slack.api('channels.invite', {
-                    channel: result.channel.id,
-                    user: userList['josiahtest'].id
-                }, function(err, inviteResponse){
-                    console.log('channels.invite response: %j', inviteResponse);
-                    cb(null, inviteResponse);
+                async.each(usersToInvite, function(userToInvite, callback) {
+                //slackUsers = _.indexBy(response.members, 'name');
+                
+                    var slackUser = _.findWhere(slackUsers, { 'name': userToInvite });
+                    if (slackUser === undefined) {
+                        console.log('Unable to find the user %j', userToInvite);
+                        callback();
+                    }
+
+                    slack.api('channels.invite', {
+                        channel: result.channel.id,
+                        user: slackUser.id
+                    }, function(err, inviteResponse) {
+                        if (err) {
+                            console.log('Unable to invite %j with error: %j', slackUser.id, err);
+                        } else {
+                            console.log('channels.invite response: %j', inviteResponse);
+                        }
+                        callback();
+                    });
+                }, function(err) {
+                    cb(null, result);
                 });
-            }
+            },
         ], function(err, result) {
             var texts = [
                 'That’s a solid crew you’ve assembled there.',
@@ -71,7 +110,7 @@ module.exports = BaseCmd.extend({
             ];
 
             var randId = _.random(0,1);
-            this.response.send(texts[randId]);
+            commandResponseObj.send(texts[randId]);
         });
     }
 });
