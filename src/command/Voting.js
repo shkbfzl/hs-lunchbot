3b933c4a0cf329109e4a151292d27cb0c518d039
Taylor Johndrew <johndrew.t@gmail.com>
2016-09-27 18:57:40 -0700

    Added tests and methods to voting command.

--- src/command/Voting.js
+++ src/command/Voting.js
@@ -21,6 +21,67 @@ module.exports = BaseCmd.extend({
     run: function() {
 
         this.response.send("Hello, are you hungry?");
+    },
+
+    getOutput: function() {
+    	var template = _.template(`Congratulations! Your team has picked a lunch.
+							You will be lunching at <%= place %>.`);
+    	var place = this.getResult();
+
+    	return template({place:place});
+    },
+
+    /**
+     * Retrieves the place with the least number of tallies.
+     * 
+     * @return {String} The name of a place
+     */
+    getResult: function() {
+    	var places = this.getPlaces();
+    	var choices = this.getChoices();
+    	var tallies = this.tally(places, choices);
+    	var result;
+
+    	_.each(_.keys(tallies), function(place) {
+    		if (!result) {
+    			result = place;
+    		} else {
+    			result = (tallies[place] < tallies[result]) ? place : result;
+    		}
+    	});
+
+    	return result;
+    },
+
+    getPlaces: function() {},
+    getChoices: function() {},
+
+    /**
+     * Takes three lunch options and an object containing users' PickNot choices, 
+     * and returns an object with the tallies for each place.
+     * 
+     * @param  {Object} places  Contains the three calculated places.
+     * @param  {Array} choices  List of usernames mapped to a PickNot choice.
+     * @return {Object}         Final tallies.
+     */
+    tally: function(places, choices) {
+    	if (Object.keys(places).length > 3) {
+    		throw new Error('Cannot have more than three places.');
+    	}
+    	var tallies = {};
+
+    	_.each(_.values(places), function(v) {
+    		tallies[v] = 0;
+    	});
+
+    	_.each(choices, function(choice) {
+    		if (choice.choice) {
+    			var place = places[choice.choice];
+    			tallies[place] += 1;
+    		}
+    	});
+
+    	return tallies;
     }
 });
 
--- test/specs/command/Voting.spec.js
+++ test/specs/command/Voting.spec.js
@@ -1,6 +1,7 @@
 require('rootpath')();
 
 var assert = require('chai').assert;
+var sinon = require('sinon');
 var VotingCommand = require('src/command/Voting');
 
 describe('Voting Command test', function() {
@@ -10,18 +11,160 @@ describe('Voting Command test', function() {
 		command = new VotingCommand();
 	});
 
-	describe('Tests getPlaces method');
+	describe('Tests getPlaces method', function() {
+		
+	});
 
-	describe('Tests getUserChoices method');
+	describe('Tests getUserChoices method', function() {
+
+	});
 
 	describe('Tests tally method', function() {
 
-		it('Returns first choice when no users have voted');
+		it('Returns an error when there are more than three places', function() {
+			var places = getTestPlaces();
+			places["4"] = "testPlace004";
+			var choices = [
+				{user:'testUser001', choice:"1"},
+				{user:'testUser002', choice:"3"},
+				{user:'testUser003', choice:"1"}
+			];
 
-		it('Returns the place with the least tallies');
+			assert.throws(function() {
+				command.tally(places, choices);
+			}, Error, 'Cannot have more than three places.');
+		});
 
-		it('Returns the first choice in case of a 2-way tie');
+		it('Returns tallies when no users have voted', function() {
+			var choices = [
+				{user:'testUser001', choice:null},
+				{user:'testUser002', choice:null},
+				{user:'testUser003', choice:null}
+			];
+			var expected = {
+				"testPlace001": 0,
+				"testPlace002": 0,
+				"testPlace003": 0
+			};
+
+			assert.deepEqual(command.tally(getTestPlaces(), choices), expected);
+		});
+
+		it(`Returns tallies when one place has most votes 
+			and one place has no votes`, function() {
+			var choices = [
+				{user:'testUser001', choice:"1"},
+				{user:'testUser002', choice:"3"},
+				{user:'testUser003', choice:"1"}
+			];
+			var expected = {
+				"testPlace001": 2,
+				"testPlace002": 0,
+				"testPlace003": 1
+			};
+
+			assert.deepEqual(command.tally(getTestPlaces(), choices), expected);
+		});
+
+		it('Returns tallies when there is a two-way tie', function() {
+			var choices = [
+				{user:'testUser001', choice:"3"},
+				{user:'testUser002', choice:"3"},
+				{user:'testUser003', choice:"3"}
+			];
+			var expected = {
+				"testPlace001": 0,
+				"testPlace002": 0,
+				"testPlace003": 3
+			};
+
+			assert.deepEqual(command.tally(getTestPlaces(), choices), expected);
+		});
+
+		it('Returns tallies when there is a three-way tie', function() {
+			var choices = [
+				{user:'testUser001', choice:"1"},
+				{user:'testUser002', choice:"2"},
+				{user:'testUser003', choice:"3"}
+			];
+			var expected = {
+				"testPlace001": 1,
+				"testPlace002": 1,
+				"testPlace003": 1
+			};
+
+			assert.deepEqual(command.tally(getTestPlaces(), choices), expected);
+		});
 	});
 
-	describe('Tests getResult method');
+	describe('Tests getResult method', function() {
+		var tallyMethodStub;
+		var getPlacesMethodStub;
+		var getChoicesMethodStub;
+
+		beforeEach(function() {
+			tallyMethodStub = sinon.stub(command, "tally");
+			getPlacesMethodStub = sinon.stub(command, "getPlaces");
+			getChoicesMethodStub = sinon.stub(command, "getChoices");
+		});
+
+		it('Returns the place with the least tallies', function() {
+			tallyMethodStub.returns({
+				"testPlace001": 2,
+				"testPlace002": 0,
+				"testPlace003": 1
+			});
+			var expected = "testPlace002";
+
+			assert.equal(command.getResult(), expected);
+		});
+
+		it('Returns the first place in a three-way tie', function() {
+			tallyMethodStub.returns({
+				"testPlace001": 1,
+				"testPlace002": 1,
+				"testPlace003": 1
+			});
+			var expected = "testPlace001";
+
+			assert.equal(command.getResult(), expected);
+		});
+
+		it('Returns the topmost place in a two-way tie', function() {
+			tallyMethodStub.returns({
+				"testPlace001": 3,
+				"testPlace002": 0,
+				"testPlace003": 0
+			});
+			var expected = "testPlace002";
+
+			assert.equal(command.getResult(), expected);
+
+		});
+	});
+
+	describe('Tests getOutput method', function() {
+		var getResultMethodStub;
+
+		beforeEach(function() {
+			getResultMethodStub = sinon.stub(command, "getResult");
+		});
+
+		it('Returns output with result place inserted', function() {
+			getResultMethodStub.returns("testPlace001");
+			var expected = `Congratulations! Your team has picked a lunch.
+							You will be lunching at testPlace001.`;
+
+			assert.equal(command.getOutput(), expected);
+		});
+	});
 });
+
+function getTestPlaces() {
+	return  {
+				"1": "testPlace001",
+				"2": "testPlace002",
+				"3": "testPlace003"
+			};
+			
+}
